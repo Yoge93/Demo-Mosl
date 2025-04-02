c4b46914094d549d4076a036e20200b84449f215
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-02 15:56:09 +0530

    added card01 block

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,24 @@
+import Swiper from "../carousel/swiper-bundle.min.js"
+
+export default function decorate(block) {
+    console.log(block)
+    block.classList.add('swiper')
+    const Div = document.createElement('div')
+    Div.classList.add('swiper-wrapper')
+    Array.from(block.children).forEach((item) => {
+        item.classList.add('swiper-slide')
+        Div.append(item)
+    })
+    block.append(Div)
+    Swiper(block, {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 1000,
+        },
+    })
+
+
+
+
+}
