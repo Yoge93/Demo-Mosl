1cbdd2090ffdfb465e550fa4b1a072e40f3a0ed7
AnuragPandey <anurag.pandey@teknopoint.in>
2025-04-03 18:59:43 +0530

    added swiper js and css

--- blocks/Navtitle/navtitle.js
+++ blocks/Navtitle/navtitle.js
@@ -1,4 +1,4 @@
-import Swiper from "./swiper-bundle-min.js";
+import Swiper from "../swiper-bundle-min.js";
 
 export default function decorate(block) {
   document.querySelector(".img-swiper-parent").classList.add("swiper");
