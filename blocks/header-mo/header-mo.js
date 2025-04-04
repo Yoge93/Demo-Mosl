13817de4ef4e90713e643c8fcdca96d253eff82c
ashwin27jethawa <ashwin.jethawa@deptagency.com>
2025-04-04 12:00:32 +0530

    header mo block added

---
+++ blocks/header-mo/header-mo.js
@@ -0,0 +1,25 @@
+export default function decorate(block) {
+    Array.from(block.children).forEach((row) => {
+        row.classList.add("header-container")
+        Array.from(row.children).forEach((column) => {
+            column.classList.add("header-container-column")
+        })
+    })
+    const blockdrop = block.querySelectorAll(".header-container-column ul")
+    Array.from(blockdrop).forEach((column, index) => {
+        column.classList.add("header-container-ul-" + (index + 1))
+    })
+
+    const selectDiv = document.createElement("select");
+    selectDiv.classList.add("selectDrowpDown");
+    const ul = block.querySelectorAll(".header-container-ul-1 li");
+    ul.forEach(li => {
+        const option = document.createElement('option');
+        option.value = li.textContent; // Use the text as the value
+        option.textContent = li.textContent; // Display the text in the option
+        selectDiv.appendChild(option); // Append the option to the <select>
+    });
+    block.querySelector(".header-container-ul-1").innerHTML = "";
+    block.querySelector(".header-container-ul-1").appendChild(selectDiv)
+    // block.querySelector(".header-container-ul-1 ul li").style.display = "none"
+}
