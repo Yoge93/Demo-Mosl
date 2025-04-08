5809d68e740f68d64cedd47443a99738b2e35974
AnuragPandey <anurag.pandey@teknopoint.in>
2025-04-08 11:01:21 +0530

    css class added

--- blocks/explore-products-right-cards/explore-products-right-cards.js
+++ blocks/explore-products-right-cards/explore-products-right-cards.js
@@ -1,3 +1,9 @@
-export default function decorate(block){
-    console.log(block)
-}
+export default function decorate() {
+  var rightCards = document.querySelector(".explore-products-right-cards");
+  Array.from(rightCards.children).forEach((e, i) => {
+    e.classList.add("right-card", `right-card-${i + 1}`);
+    Array.from(e.children).forEach((e, i) => {
+      e.classList.add(`card-item-${i + 1}`);
+    });
+  });
+};
