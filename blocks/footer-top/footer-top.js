f53fd10e535139984cec9fc4fd98328bddb01884
AnuragPandey <anurag.pandey@teknopoint.in>
2025-04-09 16:12:29 +0530

    file added

--- blocks/footer-top/footer-top.js
+++ blocks/footer-top/footer-top.js
@@ -1,7 +1,7 @@
-const classNames = ["footer-content-left", "footer-content-right"];
+const classNames = ["footer-top-left", "footer-top-right"];
 
-const footerContent = document.querySelector(".footer-content").children;
-[...footerContent].forEach((e, i) => {
+const footerTop = document.querySelector(".footer-top").children;
+[...footerTop].forEach((e, i) => {
   e.classList.add(classNames[i], "footer-content");
   Array.from(e.children).forEach((e) => {
     e.classList.add("footer-list");
