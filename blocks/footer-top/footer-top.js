180f02c3707ea0b0735abccb90b501c06de30ebd
AnuragPandey <anurag.pandey@teknopoint.in>
2025-04-10 12:01:49 +0530

    new block adde

--- blocks/footer-top/footer-top.js
+++ blocks/footer-top/footer-top.js
@@ -1,7 +1,7 @@
 const classNames = ["footer-top-left", "footer-top-right"];
 
 const footerTop = document.querySelector(".footer-top").children;
-[...footerTop].forEach((e, i) => {
+Array.from(footerTop).forEach((e, i) => {
   e.classList.add(classNames[i], "footer-content");
   Array.from(e.children).forEach((e) => {
     e.classList.add("footer-list");
