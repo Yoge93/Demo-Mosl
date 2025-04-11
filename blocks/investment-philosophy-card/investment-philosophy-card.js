90d711ba61a0608f1ede44da39d511b61c604a91
AnuragPandey <anurag.pandey@teknopoint.in>
2025-04-11 11:17:23 +0530

    added new block

--- blocks/investment-philosophy-card/investment-philosophy-card.js
+++ blocks/investment-philosophy-card/investment-philosophy-card.js
@@ -1,7 +1,7 @@
-Array.from(document.querySelector(".strategy-construct-card").children).forEach(
-  (e, i) => {
-    e.classList.add(`card-item-${i + 1}`, 'card-item');
-    Array.from(e.children)[0].classList.add('item-icon')
-    Array.from(e.children)[1].classList.add('item-content')
-  }
-);
+document.querySelectorAll(".investment-philosophy-card").forEach((e, i) => {
+  Array.from(e.children).forEach((e) => {
+    e.classList.add('card-item');
+    Array.from(e.children)[0].classList.add("item-icon");
+    Array.from(e.children)[1].classList.add("item-content");
+  });
+});
