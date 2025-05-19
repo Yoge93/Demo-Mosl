3d08fc2f297603f481151b6e1c18b8225d5014db
AnuragPandey <anurag.pandey@teknopoint.in>
2025-05-19 12:28:21 +0530

    js

--- blocks/lumpsum-calculator/lumpsum-calculator.js
+++ blocks/lumpsum-calculator/lumpsum-calculator.js
@@ -1,5 +1,5 @@
 import { div, h3, form, label, input, span } from "../../scripts/dom-helper.js";
-import chart from "../lumpsum-calculator/chart.js";
+import {} from "../lumpsum-calculator/chart.js";
 
 // Function form creating Input Fields and Range
 function createField(id, labelText, min, max, value) {
@@ -62,5 +62,210 @@ lumpsumCalc.appendChild(lumpsumCalcContent);
 
 
 // Calc Functionality
-console.log(chart);
+const numberInp = document.querySelectorAll(".each-field-top input");
+const rangeInp = document.querySelectorAll(".each-field-bottom input");
+const investAmt = document.getElementById("invest-amount");
+const estReturns = document.getElementById("est-returns");
+const totalAmt = document.getElementById("total-amount");
 
+function updateFill(range) {
+  const min = range.min;
+  const max = range.max;
+  const val = range.value;
+  const getPercentage = ((val - min) / (max - min)) * 100;
+  range.style.setProperty("--progress", `${getPercentage}%`);
+  // fill.style.width = `${getPercentage}%`;
+};
+
+function inpRangeSync(e) {
+  // numberInp.forEach((numInp, i) => {
+  // const range = rangeInp[i];
+  const wrapper = e.target.closest(".each-field");
+  const numberInput = wrapper.querySelector('input[type="number"]');
+  const rangeInput = wrapper.querySelector('input[type="range"]');
+  const min = Number(e.target.min);
+  const max = Number(e.target.max);
+  const val = Number(e.target.value);
+  const type = e.target.type;
+  if (type == "number") {
+    if (min > val || isNaN(val)) {
+      rangeInput.value = min;
+      wrapper.querySelector("input").classList.add("error");
+      wrapper.querySelector("input").classList.remove("default");
+    } else if (max < val) {
+      rangeInput.value = max;
+      wrapper.querySelector("input").classList.add("error");
+      wrapper.querySelector("input").classList.remove("default");
+    } else {
+      rangeInput.value = val;
+      wrapper.querySelector("input").classList.remove("error");
+      wrapper.querySelector("input").classList.add("default");
+    }
+  } else {
+    numberInput.value = rangeInput.value;
+    wrapper.querySelector("input").classList.remove("error");
+    wrapper.querySelector("input").classList.add("default");
+  }
+};
+
+function handleInput(e) {
+  // debugger;
+  inpRangeSync(e);
+  rangeInp.forEach((inp) => {
+    updateFill(inp);
+  });
+};
+
+function calculateLumpSum(p, r, n) {
+  const calcultedValue = p * Math.pow(1 + r / 100, n);
+  const totalInvestment = Math.round(calcultedValue);
+  return totalInvestment;
+};
+
+// Rendering Value
+function renderCalculatedValue() {
+  const investAmntInp = Number(
+    document.getElementById("investment-number").value
+  );
+  const expectRtInp = Number(document.getElementById("expected-rate").value);
+  const investprdInp = Number(document.getElementById("invest-period").value);
+  const result = calculateLumpSum(investAmntInp, expectRtInp, investprdInp);
+  investAmt.textContent = investAmntInp;
+  estReturns.textContent = result - investAmntInp;
+  totalAmt.textContent = result;
+};
+
+// On load Logic
+document.addEventListener("DOMContentLoaded", function () {
+  rangeInp.forEach((inp, i) => {
+    inp.value = numberInp[i].value;
+    updateFill(inp);
+  });
+  renderCalculatedValue();
+  piechart(
+    Number(investAmt.textContent.replace(/,/g, "")),
+    Number(estReturns.textContent.replace(/,/g, ""))
+  );
+});
+
+// Input logic
+numberInp.forEach((ele) => {
+  ele.addEventListener("input", function (e) {
+    // debugger;
+    handleInput(e);
+    // rendering logic for number input
+    const numMin = Number(e.target.min);
+    const numMax = Number(e.target.max);
+    const numval = Number(e.target.value);
+    if (numval > numMax || numval < numMin) {
+      const investMin = Number(
+        document.getElementById("investment-number").min
+      );
+      const rateMin = Number(document.getElementById("expected-rate").min);
+      const periodMin = Number(document.getElementById("invest-period").min);
+      const result = calculateLumpSum(investMin, rateMin, periodMin);
+      investAmt.textContent = investMin;
+      estReturns.textContent = result - investMin;
+      totalAmt.textContent = result;
+      piechart(
+        Number(investAmt.textContent.replace(/,/g, "")),
+        Number(estReturns.textContent.replace(/,/g, ""))
+      );
+    } else {
+      renderCalculatedValue();
+      piechart(
+        Number(investAmt.textContent.replace(/,/g, "")),
+        Number(estReturns.textContent.replace(/,/g, ""))
+      );
+    }
+  });
+});
+
+// Range Logic
+rangeInp.forEach((ele) => {
+  ele.addEventListener("input", function (e) {
+    handleInput(e);
+    renderCalculatedValue();
+    piechart();
+    piechart(
+      Number(investAmt.textContent.replace(/,/g, "")),
+      Number(estReturns.textContent.replace(/,/g, ""))
+    );
+  });
+});
+
+
+function piechart(invest, estimated) {
+  Highcharts.chart("calculate-chart", {
+    chart: {
+      type: "pie",
+      backgroundColor: "transparent",
+      
+    },
+    title: {
+      text: "Lump Sum Investment Breakdown",
+      style: {
+        color: "#333",
+        fontSize: "18px",
+        fontWeight: "bold",
+        fontFamily: "Arial, sans-serif",
+      },
+    },
+    tooltip: {
+      backgroundColor: "#fff",
+      borderRadius: 3,
+      borderWidth: 1,
+      style: {
+        color: "#2b238c",
+        fontSize: "13px",
+        fontWeight: "700",
+        fontFamily: "Verdana, sans-serif",
+      },
+      shadow: true,
+      padding: 8,
+      formatter: function () {
+        return `<b>${this.point.name}</b>: ₹${this.point.y}`;
+      },
+    },
+    legend: {
+      enabled: true,
+      align: "center",
+      verticalAlign: "bottom",
+      layout: "horizontal",
+      itemStyle: {
+        fontSize: "14px",
+        color: "#333",
+      },
+    },
+    plotOptions: {
+      pie: {
+        innerSize: "70%",
+        size: "100%",
+        dataLabels: {
+          enabled: true,
+          format: "<b>{point.name}</b>: ₹{point.y}",
+          connectorWidth: 0,
+          style: {
+            color: "#000",
+            fontSize: "13px",
+            fontWeight: "normal",
+            display: "none",
+          },
+        },
+        borderWidth: 1,
+        borderColor: "#fff",
+      },
+    },
+    colors: ["#2b2e8c", "#1f88ce"], // green and orange
+    series: [
+      {
+        name: "Amount",
+        colorByPoint: true,
+        data: [
+          { name: "Invested Amount", y: Number(invest) },
+          { name: "Estimated Returns", y: Number(estimated) },
+        ],
+      },
+    ],
+  });
+}
