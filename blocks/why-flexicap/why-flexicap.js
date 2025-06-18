01aec4c27d7dd580fa3a979219ad09e7497cf018
AnuragPandey <anurag.pandey@teknopoint.in>
2025-06-18 16:15:31 +0530

    added

--- blocks/why-flexicap/why-flexicap.js
+++ blocks/why-flexicap/why-flexicap.js
@@ -1,12 +1,30 @@
-export default function (block) {
-  console.log(block);
+import Swiper from "../why-flexicap/swiper.min.js";
+export default function decorate(block) {
+  block.classList.add("swiper-wrapper");
+  block.closest(".why-flexicap-wrapper").classList.add("swiper");
   Array.from(block.children).forEach((el, i) => {
-    el.classList.add("flexicap-inner-content");
+    el.classList.add("flexicap-inner-content", "swiper-slide");
     Array.from(el.children).forEach((el, i) => {
       el.classList.add(`flexicap-subinner-${i}`);
       Array.from(el.children).forEach((el) => {
         el.classList.add("subinner-content");
       });
     });
+  });
+
+  // Initialize Swiper
+  Swiper("block.why-flexicap", {
+    // loop: true,
+    direction: "vertical"
+    //   navigation: {
+    //     nextEl: rightArrow,
+    //     prevEl: leftArrow,
+    //   },
+    //   pagination: {
+    //     // el: divPagination,
+    //     clickable: true,
+    //   },
+    // freeMode: true,
+    // scrollOnFocus: true,
   });
 }
