573b5621fa891d412d17a783f2340f1aa6293704
AnuragPandey <anurag.pandey@teknopoint.in>
2025-06-19 12:12:56 +0530

    development

--- blocks/why-flexicap/why-flexicap.js
+++ blocks/why-flexicap/why-flexicap.js
@@ -11,20 +11,59 @@ export default function decorate(block) {
       });
     });
   });
+  // Create navigation & pagination container
+  const flexicapWrapper = document.querySelector(".why-flexicap-wrapper");
 
+  const divPagination = document.createElement("div");
+  divPagination.classList.add("swiper-pagination");
+  flexicapWrapper.appendChild(divPagination);
+
+//   const leftArrow = document.createElement("div");
+//   leftArrow.classList.add("swiper-button-prev");
+//   flexicapWrapper.appendChild(leftArrow);
+
+//   const rightArrow = document.createElement("div");
+//   rightArrow.classList.add("swiper-button-next");
+//   flexicapWrapper.appendChild(rightArrow);
+
+  // Store the custom pagination content (e.g., image or text elements)
+  const paginationTexts = [];
+  Array.from(block.children).forEach((el) => {
+    console.log(el);
+    const paginationContent = el?.firstElementChild?.firstElementChild;
+    if (paginationContent) {
+      paginationTexts.push(paginationContent);
+    }
+  });
+
+  //  Now remove that pagination title from dom
+  block.querySelectorAll(".flexicap-subinner-0").forEach((el) => el.remove());
   // Initialize Swiper
-  Swiper("block.why-flexicap", {
+  Swiper(".why-flexicap-wrapper", {
     // loop: true,
-    direction: "vertical"
-    //   navigation: {
-    //     nextEl: rightArrow,
-    //     prevEl: leftArrow,
-    //   },
-    //   pagination: {
-    //     // el: divPagination,
-    //     clickable: true,
-    //   },
+     direction: "vertical",
+    navigation: false,
+    pagination: {
+      el: divPagination,
+      clickable: true,
+      renderBullet: function (index, className) {
+        const el = paginationTexts[index];
+        if (!el) return `<span class="${className}">•</span>`; // fallback
+
+        const clone = el.cloneNode(true); // safe clone
+        clone.classList.add("swiper-pagination-bullet");
+        return clone.outerHTML;
+      },
+    },
     // freeMode: true,
     // scrollOnFocus: true,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
   });
 }
